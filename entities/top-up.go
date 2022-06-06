5aa2f31f50c220aa74a806866c94533f881637f1
zenseslaus <zenseslaus@gmail.com>
2022-06-06 17:38:03 +0700

    Add All Atribut Entities

--- entities/top-up.go
+++ entities/top-up.go
@@ -1,4 +1,7 @@
 package entities
 
 type TopUp struct {
+	IdTopUps int
+	IdUser   int
+	Nominal  int
 }
--- entities/users.go
+++ entities/users.go
@@ -1,4 +1,9 @@
 package entities
 
 type Users struct {
+	IdUser      int
+	NamaLengkap string
+	NickName    string
+	Telp        string
+	Saldo       int
 }
